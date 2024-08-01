5013f3ff83eb1b6883848aca6c25bffd86a3dcbe
Odali Araujo <62185010+OdaliID@users.noreply.github.com>
2024-08-01 14:17:51 -0300

    Update Loader.jsx

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -2,7 +2,7 @@
 const Loader = () => {
 	return (
 		<div className='animate-pulse h-full w-2/12 flex justify-center items-center w-full '>
-			<img src="/noun_Pokeball.svg" alt="Loading" />
+			<img src="/pokebola.gif" alt="Loading" />
 		</div>
 	)
 }
